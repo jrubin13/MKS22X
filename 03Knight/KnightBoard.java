c93f76d5d40497fe54c1c3c77f96427a97ec104b
jrubin13 <jrubin@stuy.edu>
2018-02-28 02:12:18 -0500

    fixed up knightsBoard

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,20 +1,34 @@
 public class KnightBoard {
     int[][]board;
+    int rows;
+    int cols;
+    int[] x = {1, 1, -1, -1, 2, 2, -2, -2};
+    int[] y = {2, -2, 2, -2, 1, -1, 1, -1};
     public KnightBoard(int startingRows,int startingCols)  {
-	board = int[startingRows][startingCols];
+	board = new int[startingRows][startingCols];
+	rows = startingRows;
+	cols = startingCols;
+	if (rows < 0 || cols < 0) {
+	    throw new IllegalArgumentException();
+	}
+	for (int i = 0; i < rows; i++) {
+	    for (int z = 0; z < cols; z++) {
+		board[i][z] = 0;
+	    }
+	}
     }
     public String toString() {
 	String ans = "";
-	for (int i = 0; i < startingRows; i++) {
-	    for (int x = 0; x < startingCols;x++) {
-		if (board[i][x] == 0) {
+	for (int i = 0; i < rows; i++) {
+	    for (int z = 0; z < cols; z++) {
+		if (board[i][z] == 0) {
 		    ans += "_ ";
 		}
-		else if (board[i][x] >= 10) {
-		    ans = ans + board[i][x] + " ";
+		else if (board[i][z] >= 10) {
+		    ans = ans + board[i][z] + " ";
 		}
 		else {
-		    ans = ans + " " + board[i][x] + " ";
+		    ans = ans + " " + board[i][z] + " ";
 		}
 	    }
 	    ans += "\n";
@@ -22,19 +36,49 @@ public class KnightBoard {
 	return ans;
     }
     public boolean solve(int row, int col) {
-	for (int i = 0; i < startingRows; i++) {
-	    for (int x = 0; x < startingCols;x++) {
-		if (board[i][x] != 0) {
+	for (int i = 0; i < rows; i++) {
+	    for (int z = 0; z < cols; z++) {
+		if (board[i][z] != 0) {
 		    throw new IllegalStateException();
 		}
 	    }
 	}
-	if (row <= 0 || col <= 0) {
+	if (row < 0 || col < 0 || row >= rows || col >= cols) {
 	    throw new IllegalArgumentException();
 	}
-	return solveH(row, col, 1);
+	board[row][col] = 1;
+	return solveH(row, col, 2);
+    }
+    public int countSolutions(int row, int col) {
+	int total = 0;
+	for (int i = 0; i < rows; i++) {
+	    for (int z = 0; z < cols; z++) {
+		if (board[i][z] != 0) {
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	if (row < 0 || col < 0 || row >= rows || col >= cols) {
+	    throw new IllegalArgumentException();
+	}
+	board[row][col] = 1;
+        for (int i = 0; i < 8; i++) {
+	    if (solveH(row, col, 2)) {
+		total+= 1;
+	    }
+	}
+	return total;
     }
     private boolean solveH(int row ,int col, int level) {
-
+	if (level == row*col) {
+	    return true;
+	}
+	for (int i = 0; i < 8; i++) {
+	    if ((row+x[i]>=0&&row+x[i]<rows) && (col+y[i]>=0&&col+y[i]<cols) && (board[row+x[i]][col+y[i]]==0)) {
+		board[row+x[i]][col+y[i]] = level;
+		solveH(row+x[i], col+y[i], level+1);
+	    }
+	}
+	return false;
     }
 }
