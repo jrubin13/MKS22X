2e67c890a17aed2e69e4ee74098167c736894be0
jrubin13 <jrubin@stuy.edu>
2018-03-11 18:46:51 -0400

    (from friday) continued to work on bronze

---
+++ 05USACO/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[] main) {
+	USACO bronze;
+	bronze = new Bronze("data.dat");
+	System.out.println(bronze);
+	System.out.println(bronze.solve());
+	System.out.println(bronze);
+    }
+}
--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,6 +1,22 @@
 public class USACO {
     public static int bronze(String filename) {
+	int row = 0;
+	int col = 0;
+	int elevation = 0;
+	int[][] maze;
+	String numInstructions = "";
+	File text = new File(filename);// can be a path like: "/full/path/to/file.txt"
 	
+	//inf stands for the input file
+	    Scanner inf = new Scanner(text);
+	    String str = "";
+	    int i = 0;
+	    while(inf.hasNextLine()){
+		String line = inf.nextLine();
+		if (i == 0) {
+		    
+		str += line;
+	    }
     }
      public static int silver(String filename);
 }
