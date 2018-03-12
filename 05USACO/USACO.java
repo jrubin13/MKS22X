7929382287ad930f44a6cf203fdadb0d7e1e80fe
jrubin13 <jrubin@stuy.edu>
2018-03-11 21:26:19 -0400

    able to take the four data points and create the map, store elevation and number of instructions

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,22 +1,55 @@
+import java.util.*;
+import java.io.*;
 public class USACO {
     public static int bronze(String filename) {
-	int row = 0;
-	int col = 0;
-	int elevation = 0;
-	int[][] maze;
-	String numInstructions = "";
-	File text = new File(filename);// can be a path like: "/full/path/to/file.txt"
-	
-	//inf stands for the input file
+	try {
+	    int row = 0;
+	    int col = 0;
+	    int elevation = 0;
+	    int numInstructions = 0;
+	    File text = new File(filename);// can be a path like: "/full/path/to/file.txt"
+	    //inf stands for the input file
 	    Scanner inf = new Scanner(text);
-	    String str = "";
-	    int i = 0;
-	    while(inf.hasNextLine()){
-		String line = inf.nextLine();
-		if (i == 0) {
-		    
-		str += line;
+	    row = inf.nextInt();
+	    col = inf.nextInt();
+	    elevation = inf.nextInt();
+	    numInstructions = inf.nextInt();
+	    int[][] map = new int[row][col];
+	    for (int i = 0; i < row; i++) {
+		for (int x = 0; x < col; x++) {
+		    map[i][x] = inf.nextInt();
+		}
 	    }
+	    //System.out.println(row);
+	    //System.out.println(col);
+	    //System.out.println(elevation);
+	    //System.out.println(numInstructions);
+	    //System.out.println(map);
+	    return 2;
+	}
+	catch (FileNotFoundException e) {
+	    return 0;
+	}
     }
-     public static int silver(String filename);
+    public static int silver(String filename) {
+	return 0;
+    }
+    public String toString() {
+	String ans = "";
+	for (int i = 0; i < rows; i++) {
+	    for (int z = 0; z < cols; z++) {
+		if (board[i][z] == 0) {
+		    ans += " _ ";
+		}
+		else if (board[i][z] >= 10) {
+		    ans = ans + board[i][z] + " ";
+		}
+		else {
+		    ans = ans + " " + board[i][z] + " ";
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
 }
